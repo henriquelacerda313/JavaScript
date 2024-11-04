7fa24b04a77681a825d0ec6093c2683e9084d12b
henriquelacerda313 <161867548+henriquelacerda313@users.noreply.github.com>
2024-11-04 17:56:19 -0300

    atualizando repositórios 4/11

---
+++ aula 011/aula08.js
@@ -0,0 +1,15 @@
+
+
+var vel = 61
+
+
+if  (vel > 60) {
+
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+
+}
+
+
+console.log(`a velocidade do seu carro é de ${vel}km/h`)
+
+console.log('diriga sempre usando cinto de segurança!')
