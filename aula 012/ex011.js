904b43e133d261fa4d231e310851114f072c2493
henriquelacerda313 <161867548+henriquelacerda313@users.noreply.github.com>
2024-11-12 10:00:48 -0300

    calculo de votos if else

---
+++ aula 012/ex011.js
@@ -0,0 +1,11 @@
+var idade = 17
+if (idade <16) {
+
+    console.log ('não vota')
+    
+} else if(idade<18||idade>65){
+console.log('voto opcional')
+
+} else {
+    console.log('voto obrigatorio')
+}
