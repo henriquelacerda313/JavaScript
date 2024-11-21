800c6914c6ebc496b1a6e1e21f3c375d6889d36b
henriquelacerda313 <161867548+henriquelacerda313@users.noreply.github.com>
2024-11-21 15:48:16 -0300

    construindo projeto

--- 012 ex/ex015/script.js
+++ 012 ex/ex015/script.js
@@ -1,34 +1,5 @@
-function carregar() {
-
-    var msg = window.document.getElementById('msg')
-
-    var img = window.document.getElementById('foto')
-
-    var data = new Date ()
-
-   var hora = data.getHours()
-    
-
-    
-
-    msg.innerHTML =  `Agora são ${hora} horas` 
-    
-    if (hora >= 0 && hora < 12 ) 
-        {   img.src= 'manha.jpg'
-            document.body.style.background = '#ffff00'} 
-        
-        
-        
-        
-        
-        else if (hora >= 12 && hora < 18) {
-            img.src = 'tarde.jpg'
-            document.body.style.backgroud = '#ff8c00'} 
-            
-            else {
-        img.src = 'noite.jpg'
-        document.body.style.backgroud = '#4b0082'
-
-    }
+function verificar () {
+ 
+    window.alert('funcionou!')
     
 }
