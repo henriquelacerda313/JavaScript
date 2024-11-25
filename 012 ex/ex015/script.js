46fa50b64542adc9b6439b43a45ea0f24847b8d2
henriquelacerda313 <henrique.160599@hotmail.com>
2024-11-25 05:42:51 -0300

    entendendo operador || OR OU

--- 012 ex/ex015/script.js
+++ 012 ex/ex015/script.js
@@ -1,5 +1,11 @@
 function verificar () {
  
-    window.alert('funcionou!')
+    var data  = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res  = document.querySelector('div#res')
     
+    if (fano.value.length == 0 || Number(fano.value) > ano) { window.alert('[ERRO] Verifique os dados e tente novamente')}
+
+    // Operador || OU OR - Este operador retorna verdadeiro se pelo menos um dos operandos for verdadeiro. Se ambos os operandos forem falsos, então o resultado será falso. 
 }
