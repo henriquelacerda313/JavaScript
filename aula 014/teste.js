fa715aac0b768c5347f14d42dab8bb408aee6d6d
henriquelacerda313 <161867548+henriquelacerda313@users.noreply.github.com>
2024-12-09 10:05:48 -0300

    aula 014 - for

---
+++ aula 014/teste.js
@@ -0,0 +1,29 @@
+/* var contador = 1      ***WHILE
+
+while(contador <=10) {
+    console.log(`iniciando em ${contador}`)
+
+    contador ++
+}
+     */ 
+
+
+/*                **** DO WHILE
+
+var contador = 1
+
+do {
+    console.log(`iniciando em ${contador}`)
+    contador ++
+} 
+while (contador <=10) 
+
+*/
+
+
+for(var contador = 1 ;contador <=10 ;contador ++) {
+
+    console.log(`iniciando em ${contador}`)
+
+
+}
