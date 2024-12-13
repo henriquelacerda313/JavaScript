21ccf8ccd1e1bb7f326e7f1b5582446d66a6d7a4
henriquelacerda313 <161867548+henriquelacerda313@users.noreply.github.com>
2024-12-13 08:43:27 -0300

    funções

---
+++ aula 016/funcao02.js
@@ -0,0 +1,12 @@
+/* function soma(n1, n2) {
+    return n1 + n2
+}
+
+console.log(soma(2,5)) */
+
+function soma(n1=0, n2=0) {  // colocar no parâmeto  = 0 , por que se não especificar no parâmetro valores para as duas saidas, vai retornar NaN
+
+    return n1 + n2 
+}
+
+console.log(soma(2))
