ad9d13bad7a51ef2c53bc1e4b974a8e7d0d0f49f
henriquelacerda313 <henrique.160599@hotmail.com>
2025-01-09 06:48:53 -0300

    attt

--- 016 ex/script.js
+++ 016 ex/script.js
@@ -26,12 +26,30 @@ function inlista(n, l) {
 function adicionar() {
 
 if (isNumero(num.value)&& !inlista(num.value, valores)){
-    window.alert('tudo OK')
+    valores.push(Number(num.value))  /* push - adicionar */
+    let item = document.createElement('option')
+    item.text = `Valor ${num.value} foi adicionado`
+    lista.appendChild(item  )
 }
 
     else{
         window.alert('Valor inválido ou já existe')
     }
 
+    num.value = ''
+    num.focus()  /*ao adicionar o item limpa a caixa de digitação atumaticamente*/ 
 
+}
+
+
+function finalizar () {
+    if (valores.length == 0) 
+        {window.alert('Adicione valores antes de finalizar')}
+    else {
+        let total  = valores.length
+
+        res.innerHTML = ''
+        res.innerHTML +=   `'<p> ao todo temos ${}</p>  `
+
+    }
 }
