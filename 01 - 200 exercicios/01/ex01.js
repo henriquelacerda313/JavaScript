6ddf8efcca2f95b2d159d23570665eae1b03f77d
henriquelacerda313 <161867548+henriquelacerda313@users.noreply.github.com>
2025-01-13 16:07:36 -0300

    desafio 200 -1e 2

---
+++ 01 - 200 exercicios/01/ex01.js
@@ -0,0 +1,6 @@
+/*Exercício 1: Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.*/
+
+let idade = 30
+let peso  = 80
+
+console.log(`Henrique tem a idade de ${idade} anos e pesa aproximadamente ${peso}kg`)
---
+++ 01 - 200 exercicios/01/ex02.js
@@ -0,0 +1,5 @@
+/*Declare uma constante chamada "PI", atribua o valor de Pi a ela e imprima o valor na console.*/ 
+
+const pi = Math.PI
+
+console.log(`o valor de PI é ${pi}`)
