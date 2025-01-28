353bc72b4880aded723501b9e74893c7e5420d99
henriquelacerda313 <161867548+henriquelacerda313@users.noreply.github.com>
2025-01-28 09:33:09 -0300

    exercícios 28/1

---
+++ 01 - 200 exercicios/02 - Operadores Condicionais/ex017.js
@@ -0,0 +1,15 @@
+/*
+Exercício 17: Escreva um programa que determina se um número é par ou ímpar.
+
+***minha tentativa abaixo**
+
+*/
+
+function parOUimpar(par){
+     if(par % 2 ==0) {console.log('este numero é par ')} else{
+        console.log('este é impar ')
+     }
+}
+
+parOUimpar(8);
+parOUimpar(11);
---
+++ 01 - 200 exercicios/02 - Operadores Condicionais/ex018.js
@@ -0,0 +1,20 @@
+/* 
+Exercício 18: Escreva um programa que verifica se uma pessoa pode dirigir com base na idade.
+
+**minha tentativa***
+
+*/
+
+function verificadorIdade (idade) {
+
+if(idade>= 18) {
+    console.log('A pessoa tem idade minima para habilitação de motorista')
+
+} else{
+    console.log('a pessoa NÃO tem idade minima para habilitação de motorista')
+}
+
+}
+
+verificadorIdade(21);
+verificadorIdade(15);
