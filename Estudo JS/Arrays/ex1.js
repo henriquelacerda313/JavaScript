f3290789bf4dd5cd2f37509ee5dcd2c4f44a975d
henriquelacerda313 <henrique.160599@hotmail.com>
2025-10-19 23:25:29 -0300

    Arrays

---
+++ Estudo JS/Arrays/ex1.js
@@ -0,0 +1,13 @@
+const listaDeFrutas = ["Maçã", "Banana", "Laranja", "Uva"];
+
+console.log(listaDeFrutas[0]); // Resultado: "Maçã"
+console.log(listaDeFrutas[2]); // Resultado: "Laranja"
+
+
+  /*
+  Arrays
+
+  Um Array (ou "vetor") em JavaScript é como uma lista ou um contêiner que permite armazenar vários valores em uma única variável.
+
+  Índice (As gavetas numeradas): Cada item dentro do array tem uma posição, que chamamos de índice. O detalhe mais importante é que a contagem sempre começa do zero.
+  */
---
+++ Estudo JS/Arrays/ex2.js
@@ -0,0 +1,12 @@
+const arrayMisto = ["Texto", 42, true, { nome: "Objeto" }];
+
+console.log(arrayMisto[0]);
+console.log(arrayMisto[1]);
+console.log(arrayMisto[2]);
+console.log(arrayMisto[3]);
+
+/*
+Um Detalhe de JavaScript:
+Em JavaScript, os arrays são flexíveis. Você pode guardar tipos de dados diferentes (números, textos, booleanos, objetos) no mesmo array, se precisar!
+
+*/
